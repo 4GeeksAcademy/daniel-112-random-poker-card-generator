c0989f45b4d7ca81330fafd176d5cbfd7e2e5e3e
Daniel <danolox@gmail.com>
2025-06-16 06:58:54 +0000

    entrega proyecto

--- src/app.js
+++ src/app.js
@@ -1,11 +1,44 @@
-import "bootstrap";
-import "./style.css";
 
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+const suits = ["♦", "♥", "♠", "♣"];
+const numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+function randomPosition(someArray) {
+
+    let arrayLength = someArray.length;
+    let randomPosition = Math.floor( Math.random() * arrayLength);
+
+    return someArray[randomPosition];
+}
+
+
+function generateNewCard() {
+
+    let randomSuit = randomPosition(suits);
+    let randomNumber = randomPosition(numbers);
+
+    let topSuitValue = document.querySelector(".top");
+    let numberValue = document.querySelector(".number");
+    let bottomSuitValue = document.querySelector(".bottom");
+
+    topSuitValue.innerHTML = randomSuit;
+    bottomSuitValue.innerHTML = randomSuit;
+
+    numberValue.innerHTML = randomNumber;
+
+
+    if (randomSuit === "♥" || randomSuit === "♦") {
+        document.querySelector(".top").style.color = "red";
+        document.querySelector(".bottom").style.color = "red";
+    } else {
+        document.querySelector(".top").style.color = "";
+        document.querySelector(".bottom").style.color = "";
+    }
 };
+
+
+// PARA QUE FUNCIONE AL CARGAR LA PÁGINA
+// window.onload = generateNewCard;
+
+// BOTÓN GENERADOR DE CARTAS
+document.getElementById("cardGeneratorButton").onclick = generateNewCard;
