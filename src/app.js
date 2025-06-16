ff91e7b1d7378ff5afc1b29982e650c58dc24d2e
Daniel <danolox@gmail.com>
2025-06-16 12:44:50 +0200

    Update app.js - "1" taken away since it does not exist in the deck

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 
 
 const suits = ["♦", "♥", "♠", "♣"];
-const numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
+const numbers = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
 
 function randomPosition(someArray) {
 
